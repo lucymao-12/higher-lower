439c0a93c5f39673c874a3cb8be02a57452cbbe9
lucymao-12 <lucymao12@gmail.com>
2023-12-27 16:18:49 -0500

    styling done, still little things to fix but much more user friendly

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,15 +1,14 @@
 import "./App.css";
 import React from "react";
 import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
 function App() {
-  const [data, setData] = useState(null); // Initialize state to store fetched dat
+  const [data, setData] = useState(null); // Initialize state to store fetched data
   const [currentPage, setCurrentPage] = useState("home");
   const [score, setScore] = useState(0);
   const [currentTrackRight, setCurrentTrackRight] = useState(null);
   const [currentTrackLeft, setCurrentTrackLeft] = useState(null);
-  useEffect(() => {
-    getData();
-  }, []);
 
   function getData() {
     fetch("http://localhost:3000")
@@ -18,8 +17,9 @@ function App() {
         //console.log(data);
         setData(data);
         setCurrentTrackLeft(data.pop());
-        console.log(setCurrentTrackLeft);
+        console.log(currentTrackLeft);
         setCurrentTrackRight(data.pop());
+        setCurrentPage("game");
       });
   }
 
@@ -28,11 +28,15 @@ function App() {
   }
 
   function startGame() {
-    setCurrentPage("game");
+    setData(null);
+    setScore(0);
+    setCurrentTrackLeft(null);
+    setCurrentTrackRight(null);
+    getData();
   }
 
   const handleHigher = () => {
-    if (currentTrackRight.popularity < currentTrackLeft.popularity) {
+    if (currentTrackRight.popularity >= currentTrackLeft.popularity) {
       setScore(score + 1);
       setCurrentTrackLeft(currentTrackRight);
       setCurrentTrackRight(data.pop());
@@ -42,7 +46,7 @@ function App() {
   };
 
   const handleLower = () => {
-    if (currentTrackRight.popularity > currentTrackLeft.popularity) {
+    if (currentTrackRight.popularity <= currentTrackLeft.popularity) {
       setScore(score + 1);
       setCurrentTrackLeft(currentTrackRight);
       setCurrentTrackRight(data.pop());
@@ -52,41 +56,117 @@ function App() {
   };
 
   return (
-    <div>
+    <div className="container">
       {currentPage === "home" && (
         <>
-          <button onClick={showRules}>Rules</button>
-          <button onClick={startGame}>Start</button>
+          <h1
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: "5%",
+            }}
+          >
+            Spotify higher or Lower
+          </h1>
+          <div className="button-container">
+            <button onClick={startGame} className="button btn btn-primary">
+              Start
+            </button>
+            <button onClick={showRules} className="button btn btn-primary">
+              Rules
+            </button>
+          </div>
         </>
       )}
       {currentPage === "rules" && (
         <>
-          <p>
+          <p className="rules">
             Spotify, unfortunately does not offer a monthly listens, number of
             plays, etc for a specific track but it does offer a popularity
             rating for each track. Which is a more time sensitive number that
             evaluates how many times a song has been played. This game is
-            inspired by
-            <a href="https://www.higherlowergame.com/"> this amazing game! </a>
+            inspired by{" "}
+            <a
+              className="link-no-style"
+              href="https://www.higherlowergame.com/"
+            >
+              this amazing game!
+            </a>{" "}
+            <br />
             If you think that the song on the right is more popular than the
             song on the left press higher, else lower. It's as simple as that!
           </p>
-          <button onClick={startGame}>Start</button>
+          <div className="button-container">
+            <button onClick={startGame} className="button btn btn-primary">
+              Start
+            </button>
+          </div>
         </>
       )}
       {currentPage === "game" && (
-        <>
-          <div className="leftTrack">
-            <img src={currentTrackLeft.image} alt="album cover"></img>
+        <div>
+          <div className="track-container">
+            <p className="popularity">
+              Popularity: {currentTrackLeft.popularity}
+            </p>
+
+            <div className="leftTrack">
+              <img
+                className="track"
+                src={currentTrackLeft.image}
+                alt="album cover"
+              ></img>
+              <div className="track-info">
+                <p className="info">Name: {currentTrackLeft.name}</p>
+                <p className="info">Artist: {currentTrackLeft.artist}</p>
+              </div>
+              <h2 style={{ margin: "1rem" }}>Score: {score}</h2>
+            </div>
+            <div className="rightTrack">
+              <img
+                className="track"
+                src={currentTrackRight.image}
+                alt="album cover"
+              ></img>
+              <div className="track-info">
+                <p className="info">Name: {currentTrackRight.name}</p>
+                <p className="info">Artist: {currentTrackRight.artist}</p>
+              </div>
+
+              <div className="button-container">
+                <button
+                  onClick={handleHigher}
+                  className="button btn btn-primary"
+                >
+                  Higher
+                </button>
+                <button
+                  onClick={handleLower}
+                  className="button btn btn-primary"
+                >
+                  Lower
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="rightTrack">
-            <img src={currentTrackRight.image} alt="album cover"></img>
-            <button onClick={handleHigher}>Higher</button>
-            <button onClick={handleLower}>Lower</button>
+        </div>
+      )}
+      {currentPage === "end" && (
+        <>
+          <p className="ending">You got: {score} points!</p>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <button className="button btn btn-primary" onClick={startGame}>
+              Start Again
+            </button>
           </div>
         </>
       )}
-      {currentPage === "end" && <p>{score}</p>}
     </div>
   );
 }
